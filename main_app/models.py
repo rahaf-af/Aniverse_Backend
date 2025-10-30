2ddc76436edaa4d832507e2301c1a28ee6778fe1
rahaf-af <rahaf77553@gmail.com>
2025-10-30 12:34:33 +0300

    finished adding all the project models + added the full CRUD for the post model

--- main_app/models.py
+++ main_app/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+#---------------User---------------
 class User(AbstractUser):
     class Role(models.TextChoices):
         ADMIN = "ADMIN",'Admin'
@@ -11,32 +14,68 @@ class User(AbstractUser):
     def save(self , *args, **kwargs):
         if not self.pk:
             self.role = self.base_role
-            return super().save(*args, **kwargs)
+        super().save(*args, **kwargs)
 
 class Admin(User):
     base_role =User.Role. ADMIN
-    class meta:
+    class Meta:
         proxy = True
 
+@receiver(post_save, sender = User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    else:
+        instance.profile.save()
+
 class Profile(models.Model):
-    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    user = models.OneToOneField(User,on_delete=models.CASCADE , related_name="profile")
     profile_img= models.URLField(null=True , blank=True)
-    phone_number = models.IntegerField(max_length=10 , null=True , blank=True)
+    phone_number = models.CharField(max_length=10, null=True , blank=True)
     email = models.EmailField(null=True , blank=True)
     bio = models.TextField(null=True , blank=True)
 
+
+
+#---------------Anime---------------
 class Anime (models.Model):
     publisher = models.ForeignKey(User, on_delete= models.SET_NULL , null=True , blank=True)
     title = models.CharField(max_length=50)
     poster = models.URLField( null=True , blank=True )
     genre = models.CharField(max_length=100 , default='Action')
-    rating =models.FloatField(default=0.0)
+    global_rating =models.FloatField(default=0.0)
     description = models.TextField(default= 'No description yet', null=True , blank=True)
     def __str__(self):
         return f'{self.title}'
 
+class AnimeFavorit (models.Model):
+    user = models.ForeignKey(User, on_delete= models.CASCADE , related_name="anime_favorit")
+    anime = models.ForeignKey(Anime, on_delete= models.CASCADE )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete= models.CASCADE , related_name="anime_comment")
+    anime = models.ForeignKey(Anime, on_delete= models.CASCADE )
+    text= models.CharField(max_length=100, null=True , blank=True)
+    rating =models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+
+#---------------Post---------------
 class Post (models.Model):
-    auther = models.ForeignKey(User, on_delete= models.SET_NULL , null=True , blank=True)
+    auther = models.ForeignKey(User, on_delete= models.CASCADE , related_name="auther")
     poster = models.URLField( null=True , blank=True )
     text = models.CharField(max_length=100)
-    created_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class PostFavorit (models.Model):
+    user = models.ForeignKey(User, on_delete= models.CASCADE , related_name="post_favorit")
+    post= models.ForeignKey(Post, on_delete= models.CASCADE)
+    created_at =models.DateTimeField(auto_now_add=True)
+
+class PostComment(models.Model):
+    user = models.ForeignKey(User, on_delete= models.CASCADE , related_name="anime_post")
+    anime = models.ForeignKey(Post, on_delete= models.CASCADE )
+    text= models.CharField(max_length=100, null=True , blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
--- main_app/views.py
+++ main_app/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .models import Anime , Profile
+from .models import Anime , Profile , Post
 from .serializers import Animeserializer , Profileserializer , Postserializer
 from rest_framework import status
 from django.shortcuts import get_object_or_404
 
 # Create your views here.
+
+
+
 # ---------------Anime CRUD---------------
 class AnimeIndex(APIView):
     # ---------------Read Anime---------------
@@ -51,6 +54,54 @@ class AnimeDetail(APIView):
             queryset = get_object_or_404(Anime,id=Anime_id)
             queryset.delete()
             return Response({'message': f'Anime {Anime_id} has been deleted !!! '},status =status.HTTP_204_NO_CONTENT )
+        except Exception as error: 
+            return Response({'error':str(error)},status =status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+# ---------------Post CRUD---------------
+class PostIndex(APIView):
+    # ---------------Read Post---------------
+    def get(self, request ):
+        queryset = Post.objects.all()
+        serializer = Postserializer(queryset, many=True)
+        return Response(serializer.data)
+    
+    # ---------------Create Post---------------
+    def post(self, request):
+        try:
+            serializer = Postserializer(data= request.data)
+            if serializer.is_valid():
+               serializer.save()
+               return Response(serializer.data,status =status.HTTP_201_CREATED )
+            return Response(serializer.errors,status =status.HTTP_400_BAD_REQUEST )
+        except Exception as error: 
+            return Response({'error':str(error)},status =status.HTTP_500_INTERNAL_SERVER_ERROR )
+
+class PostDetail(APIView):
+    # ---------------Post Detail---------------
+    def get(self, request, Post_id):
+        queryset = get_object_or_404(Post,id=Post_id)
+        serializer = Postserializer(queryset)
+        return Response(serializer.data)
+    
+    # ---------------Update Post---------------
+    def put(self, request, Post_id):
+        try:
+            queryset = get_object_or_404(Post,id=Post_id)
+            serializer = Postserializer(queryset, data= request.data)
+            if serializer.is_valid():
+               serializer.save()
+               return Response(serializer.data,status =status.HTTP_200_OK )
+            return Response(serializer.errors,status =status.HTTP_400_BAD_REQUEST )
+        except Exception as error: 
+            return Response({'error':str(error)},status =status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+    # ---------------Delete Post---------------
+    def delete(self, request, Post_id):
+        try:
+            queryset = get_object_or_404(Post,id=Post_id)
+            queryset.delete()
+            return Response({'message': f'Post {Post_id} has been deleted !!! '},status =status.HTTP_204_NO_CONTENT )
         except Exception as error: 
             return Response({'error':str(error)},status =status.HTTP_500_INTERNAL_SERVER_ERROR)
 
